b86a2b6d6348c7c237c8343b99aa2e1a09d7b999
mohanagarwal0910 <mohanagarwal09101@gmail.com>
2025-03-05 16:14:15 +0530

    [Mohan]add.UC2

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Hello from BridgeLabz';
+  imgUrl="https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75";
+}
