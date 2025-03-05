4427b39574dfa662709e92bc45a9f3fcf133a88e
mohanagarwal0910 <mohanagarwal09101@gmail.com>
2025-03-05 16:42:54 +0530

    [Mohan]add.UC3

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'BindingTechnique' title`, () => {
+  it(`should have the 'EventBindingTechnique' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BindingTechnique');
+    expect(app.title).toEqual('EventBindingTechnique');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, BindingTechnique');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, EventBindingTechnique');
   });
 });
--- app.component.ts
+++ app.component.ts
@@ -8,6 +8,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hello from BridgeLabz';
+  title = 'HelloWorld';
   imgUrl="https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75";
+url="https:/www.bridgelabz.com";
+ngOnInit(): void {
+  this.title= "Hello from BridgeLabz";
+}
+onClick($event:any){
+  console.log("Save button is clicked!",$event);
+  window.open(this.url,"_blank");
+
+}
 }
