e1aa33d608baa252fce92fc9e45b7e8975ad1727
mohanagarwal0910 <mohanagarwal09101@gmail.com>
2025-03-06 11:24:17 +0530

    [Mohan]add.UC5_UserNameValid

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'UserInputName' title`, () => {
+  it(`should have the 'UserNameValid' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('UserInputName');
+    expect(app.title).toEqual('UserNameValid');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UserInputName');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UserNameValid');
   });
 });
--- app.component.ts
+++ app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
     imgUrl="https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75";
   url="https:/www.bridgelabz.com";
   userName: string="";
+  nameError:string="";
   ngOnInit(): void {
     this.title= "Hello from BridgeLabz";
   }
@@ -19,6 +20,15 @@ export class AppComponent {
     console.log("Save button is clicked!",$event);
     window.open(this.url,"_blank");
   }
+  onInput($event:any){
+    console.log("Change Event Occured!",$event.data);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect";
+
+  }
   }
   
-
