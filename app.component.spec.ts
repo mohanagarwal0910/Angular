f093dfb2ff44dc5b70b6ba67ed65987e8bf8034e
mohanagarwal0910 <mohanagarwal09101@gmail.com>
2025-03-06 10:55:43 +0530

    [Mohan]add.UC4_UserName

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'EventBindingTechnique' title`, () => {
+  it(`should have the 'UserInputName' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('EventBindingTechnique');
+    expect(app.title).toEqual('UserInputName');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, EventBindingTechnique');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UserInputName');
   });
 });
--- app.component.ts
+++ app.component.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'HelloWorld';
-  imgUrl="https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75";
-url="https:/www.bridgelabz.com";
-ngOnInit(): void {
-  this.title= "Hello from BridgeLabz";
-}
-onClick($event:any){
-  console.log("Save button is clicked!",$event);
-  window.open(this.url,"_blank");
+  title = 'UserInputName';
+    imgUrl="https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75";
+  url="https:/www.bridgelabz.com";
+  userName: string="";
+  ngOnInit(): void {
+    this.title= "Hello from BridgeLabz";
+  }
+  onClick($event:any){
+    console.log("Save button is clicked!",$event);
+    window.open(this.url,"_blank");
+  }
+  }
+  
 
-}
-}
